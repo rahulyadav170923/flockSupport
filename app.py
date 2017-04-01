88b4f853cf95ad4aa0528153f1074592ce74dd61
Ashish Goel <ashishg656@gmail.com>
2017-04-01 21:27:43 +0530

    chat added

--- app.py
+++ app.py
@@ -1,10 +1,39 @@
-from flask import Flask, json
+from flask import Flask, json,jsonify,render_template
 app = Flask(__name__)
+from flask import request
+import pprint
 
 @app.route("/",methods=['GET','POST'])
 def install():
-    return "installed"
+    return render_template('home.html')
 
+@app.route('/freshdesk_webhook',methods=['POST','GET'])
+def freshdesk_webhook():
+    content = request.get_json(silent=True)
+
+    # my_json =  json.dumps(content, indent=4, sort_keys=True)
+    data = content['data']
+    data = data['item']
+    data = data['conversation_parts']
+    data = data['conversation_parts']
+    data = data[0]
+    
+    body = data['body']
+    attachment_link = None
+    attachment_name = None
+    try:
+    	attachment = data['attachments']
+    	attachment = attachment[0]
+    	attachment_name = attachment['name']
+    	attachment_link = attachment['url']
+    except:
+    	pass
+
+    print body
+    print attachment_name
+    print attachment_link
+
+    return "done"
 
 if __name__ == "__main__":
     app.run(debug=True)
